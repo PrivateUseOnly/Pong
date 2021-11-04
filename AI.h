4f70be5affed7016c762836291d443fe2c083c3e
Kamil Mlody <32574552+PrivateUseOnly@users.noreply.github.com>
2021-11-04 10:10:11 +0100

    Lost in 2017

---
+++ AI.h
@@ -0,0 +1,15 @@
+PSEUDO AI
+
+Jeœli Single
+ jeœli p_side==1
+	ustaw timer Blue na 40ms
+	
+	jeœli ball kieruje sie w gore i jest nad paletk¹
+	wlacz timer Blue up
+	else wlacz timer Blue down
+ jeœli p_side==2
+	ustawi timer Red na 40ms	
+
+	jeœli ball kieruje sie w gore
+	wlacz timer Red up
+	else wlacz timer Red down
